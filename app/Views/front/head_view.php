194e0e7f2afd8cba9855027f5cfb139ad53dcb98
Diego-2025-web <diego.dechat974@gmail.com>
2025-06-04 00:12:11 -0300

    Actualizo funcionalidad en php en primer_proyecto

---
+++ app/Views/front/head_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+<title>Página Principal </title>
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+<link href="assets/css/styles.css" rel="stylesheet" />
+<link rel="STYLESHEET" href="./assets/css/miestilo.css">
+</head>
+	<header>
+
+
+	<div class="container">
+	<div class="page-header">
+		 <h1 style="color: blue">Practicando en Talentos Digitales</h1>
+	</div>
+</header>
+
+<body>
